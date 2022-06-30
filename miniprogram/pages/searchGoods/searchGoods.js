3a03631daa554fed3e1363a899ffe56ff758faa5
ylsirs <y1105139166@outlook.com>
2022-06-30 08:39:21 +0800

    商品模糊搜索逻辑

--- miniprogram/pages/searchGoods/searchGoods.js
+++ miniprogram/pages/searchGoods/searchGoods.js
@@ -1,24 +1,42 @@
-
 let app = getApp()
+const db = wx.cloud.database()
 Page({
-  data: {
-    menuTop: '',
-    navHeight: ''
-  },
-  onLoad (options) {
-    // 适配值  胶囊的top值  -16(胶囊高度/2) -8(胶囊上方到通知栏为8像素)
-    let menuTop = app.globalData.menuTop
-    let navHeight = app.globalData.navBarHeight - 8
-    // console.log(menuTop, navHeight);
-    this.setData({
-      menuTop: menuTop,
-      navHeight: navHeight
-    })
-  },
+    data: {
+        menuTop: '',
+        navHeight: ''
+    },
+    onLoad(options) {
+        // 适配值  胶囊的top值  -16(胶囊高度/2) -8(胶囊上方到通知栏为8像素)
+        let menuTop = app.globalData.menuTop
+        let navHeight = app.globalData.navBarHeight - 8
+        // console.log(menuTop, navHeight);
+        this.setData({
+            menuTop: menuTop,
+            navHeight: navHeight
+        })
+    },
 
-  backTo(){
-    wx.navigateBack({
-        delta: 1
-    })
-  }
+    backTo() {
+        wx.navigateBack({
+            delta: 1
+        })
+    },
+
+    searchGoods(e) {
+        console.log(e.detail.value);
+        const _ = db.command
+        // 模糊搜索
+        db.collection('goods').where({
+                name: db.RegExp({
+                    regexp: e.detail.value,
+                    options: 'i',
+                })
+            })
+            .get({
+                success: res => {
+                    console.log(res.data);
+                },
+                fail: console.error
+            })
+    }
 })
